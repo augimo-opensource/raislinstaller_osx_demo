06dd084f1f4e38d2a9350224fafa64ada14ef901
Russell Anthony Yermal <russell@augimo.com>
2012-08-03 20:51:49 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaislinstallerOsxDemo::Application.config.secret_token = '7478f23b5b45105a838f94cde93a9196260c29055dab5c9cedd3578c8dd408de252b329780fb7d4483f6121292140b5487ade41b0f89a8db99fa8ee4e2171175'
